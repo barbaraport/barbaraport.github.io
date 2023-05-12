9d9152b390ddea8d01ba96088e22630bca3220e7
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-05-12 19:08:01 -0300

    feature (webpack, react and typescript): creating webpack, react and typescript project

---
+++ app.tsx
@@ -0,0 +1,4 @@
+function App() {
+  return <div>{"Under development!"}</div>;
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "development",
+  entry: "./main.tsx",
+  devtool: "inline-source-map",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
