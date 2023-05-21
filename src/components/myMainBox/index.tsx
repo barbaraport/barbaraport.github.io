322c80ed2641382bef4f54584fa94014989fde32
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-05-21 19:17:39 -0300

    feature (myMainBox): vertically centralizing content

--- src/components/myMainBox/index.tsx
+++ src/components/myMainBox/index.tsx
@@ -4,8 +4,15 @@ import { Text } from "../../elements/text";
 export const MyMainBox = (): JSX.Element => {
 
     return (
-        <Box sx={{backgroundColor:'pink', width:'100%', height: '100vh', display: 'flex', alignSelf: 'center'}}>
-            <Text text="hello"></Text>
+        <Box sx={{
+            backgroundColor:'pink', 
+            width:'100%', 
+            height: '100vh',
+            display: 'flex', 
+            alignSelf: 'center',
+            alignItems: 'center'
+            }}>
+            <Text text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer auctor porttitor tortor non pulvinar."></Text>
         </Box>
     );
 }
