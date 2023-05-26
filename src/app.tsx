077451c8aad435c4ea0eb40acfc1f9d3d2fccbe3
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-05-25 21:13:15 -0300

    feature (menu): creating sketch from menu component

--- src/app.tsx
+++ src/app.tsx
@@ -5,11 +5,13 @@ import { Text } from "./elements/text";
 import { MyMainBox } from "./components/myMainBox";
 import { Box, ThemeProvider } from "@mui/material";
 import theme from "./theme";
+import Menu from './components/menu'
 
 const App = (): JSX.Element => {
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{backgroundColor:'text.secondary'}}>
+        <Menu/>
         <MyMainBox/>
         <Container sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '8px'}}>
             <MyAvatar/>
