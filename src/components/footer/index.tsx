8edbbb7a97292304ea64eb71e9c8d73e4362a3f3
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-05-27 18:25:11 -0300

    fix (footer): changing width value

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,13 @@ import { Text } from "../../elements/text"
 
 export const Footer = (): JSX.Element => {
     return (
-        <Box sx={{ backgroundColor: "primary.main", width: '100vw', padding: '8px'}}>
+        <Box
+            sx={{
+                backgroundColor: "primary.main",
+                width: "100%",
+                padding: "8px",
+            }}
+        >
             <Text type="body1" text="Made with 💖 by Bárbara Port" />
         </Box>
     )
