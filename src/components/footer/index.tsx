6052ad87c582526de9d4d66001705d8eb2a0a825
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-05-27 18:20:30 -0300

    chore (footer): changing phrase

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,8 +3,8 @@ import { Text } from "../../elements/text"
 
 export const Footer = (): JSX.Element => {
     return (
-        <Box sx={{ backgroundColor: "primary.main", width: '100vw'}}>
-            <Text type="body1" text="Hello" />
+        <Box sx={{ backgroundColor: "primary.main", width: '100vw', padding: '8px'}}>
+            <Text type="body1" text="Made with 💖 by Bárbara Port" />
         </Box>
     )
 }
