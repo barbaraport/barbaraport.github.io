fbcc46d97c0b69f994846de712a6b89c2d97cc26
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-05-28 16:41:16 -0300

    Updates

--- components/aboutme/index.js
+++ components/aboutme/index.js
@@ -15,20 +15,17 @@ import { Box } from "@mui/material";
 import { MyAccordion } from "../../elements/myAccordion";
 export var AboutMe = function () {
     return (_jsxs(Box, __assign({ sx: { width: "100%" } }, { children: [_jsx(MyAccordion, { disabled: false, defaultExpanded: true, title: "About me", text: [
-                    "I am an experienced software developer contributing with software solutions of excellent quality, a thought that has accompanied me from an early age and provided me with two Honorable Mentions in educational institutions I went through.",
-                ] }), _jsx(MyAccordion, { disabled: false, defaultExpanded: false, title: "Hard Skills", text: [
-                    "Java 8 (complex business logic development);",
-                    "JUnit 5 (formulation and development of integration and unity test cases);",
-                    "Spring Framework (knowledge of microservices architecture);",
-                    "React (class and functional components development);",
-                    "TypeScript/JavaScript (client features development);",
-                    "Selenium/Cypress (consulting and development of automated tests);",
-                    "Cucumber/Gherkin (knowledge in development of automated specifications).",
-                ], paragraphIcon: _jsx(CheckBoxTwoToneIcon, {}) }), _jsx(MyAccordion, { disabled: false, defaultExpanded: false, title: "Soft Skills", text: [
-                    "Communicative (experience acquired while developing projects remotely with agile methodologies and the language cannot be a barrier);",
-                    "Self-taught (I was recognized by my team as someone who chases information to solve a problem);",
-                    "Detail-oriented (in development, in communication and in life I always pay close attention to all information I have or receive);",
-                    "Studious (I always increase my knowledge and I always search for the best trainings for this purpose).",
+                    "Experienced software developer with a passion for crafting high-quality solutions. Recognized with two Honorable Mentions for my outstanding contributions in educational institutions.",
+                ] }), _jsx(MyAccordion, { disabled: false, defaultExpanded: true, title: "Hard Skills", text: [
+                    "Complex business logic development using Java 8, formulating, and implementing integration and unit test cases with JUnit 5 and leveraging the Spring Framework for microservices architecture;",
+                    "Adept at developing React functional and class components, implementing client features using TypeScript/JavaScript;",
+                    "Consulting on automated testing with Selenium/Cypress;",
+                    "My expertise extends to Cucumber/Gherkin for automated specifications.",
+                ], paragraphIcon: _jsx(CheckBoxTwoToneIcon, {}) }), _jsx(MyAccordion, { disabled: false, defaultExpanded: true, title: "Soft Skills", text: [
+                    "My communicative nature shines through collaborating remotely using agile methodologies, breaking down language barriers;",
+                    "I'm a self-taught problem solver, continuously pursuing knowledge to overcome challenges;",
+                    "Detail-oriented in development, communication, and life, I meticulously attend to information at hand;",
+                    "With a thirst for learning, I actively seek out the best training opportunities to further enhance my abilities.",
                 ], paragraphIcon: _jsx(CheckBoxTwoToneIcon, {}) })] })));
 };
 //# sourceMappingURL=index.js.map
