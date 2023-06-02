f2cefeb92c6b02f863eb22bf9afbc8a6c8a29ea0
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-06-01 22:24:36 -0300

    work in progress (mypage): trying to add fade using ref

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -1,25 +1,42 @@
-import { Box, Container } from "@mui/material"
+import { Box, Container, Fade } from "@mui/material"
+import { useEffect, useRef, useState } from "react"
 import { AboutMe } from "../../components/aboutMe"
 import { MyAvatar } from "../../components/avatar"
 import { Footer } from "../../components/footer"
 import { MyMainBox } from "../../components/myMainBox"
 
 export const MyPage = (): JSX.Element => {
+    const myRef = useRef()
+
+    const [isMyElementVisible, updateIsMyElementVisible] = useState()
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            const entry = entries[0]
+            updateIsMyElementVisible(entry.isIntersecting)
+        })
+        observer.observe(myRef.current)
+    }, [])
+
     return (
         <Box sx={{ backgroundColor: "text.secondary" }}>
             <MyMainBox />
-            <Container
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    padding: "8px",
-                }}
-            >
-                <MyAvatar />
-                <AboutMe />
-            </Container>
-            <Footer />
+            <Fade in={isMyElementVisible} ref={myRef}>
+                <Box>
+                    <Container
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            padding: "8px",
+                        }}
+                    >
+                        <MyAvatar />
+                        <AboutMe />
+                    </Container>
+                    <Footer />
+                </Box>
+            </Fade>
         </Box>
     )
 }
