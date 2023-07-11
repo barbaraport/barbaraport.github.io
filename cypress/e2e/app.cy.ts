cbabc83c8af61c3182a38061a667b5a541ab915f
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-07-10 21:27:56 -0300

    feature: accessing my website on test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('my website', () => {
+  it('just accessing the website', () => {
+    cy.visit('http://localhost:8080')
   })
 })
